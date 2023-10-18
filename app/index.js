ec2d7b36730bd267ae0999a1511b929d6390625b
Mohamad Erfan <130228115+ErfanDev88@users.noreply.github.com>
2023-10-18 22:02:48 +0330

    Add index.js for cleaning imports link

---
+++ app/index.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import navLine from "../public/assets/Lines/navbar.png";
+import skillLine from "../public/assets/Lines/whatCanIdo.png";
+import workSamplesLine from "../public/assets/Lines/work-samples.png";
+import workSamplesLine2 from "../public/assets/Lines/workSamplesLine2.png";
+import heroImage from "../public/assets/heroImage.png";
+import mobileDeveloper from "../public/assets/mobile-developer.png";
+import designer from "../public/assets/designer.png";
+import webDeveloper from "../public/assets/web-developer.png";
+import Btn from "@/components/Btn";
+import ContinueLine from "@/components/ContinueLine";
+import WorkSampleCard from "@/components/WorkSampleCard";
+import OutlineBtn from "@/components/OutlineBtn";
+
+export {
+  Image,
+  navLine,
+  skillLine,
+  workSamplesLine,
+  workSamplesLine2,
+  heroImage,
+  mobileDeveloper,
+  designer,
+  webDeveloper,
+  Btn,
+  ContinueLine,
+  WorkSampleCard,
+  OutlineBtn,
+};
--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,20 @@
 "use client";
-import Image from "next/image";
-import navLine from "../public/assets/Lines/navbar.png";
-import skillLine from "../public/assets/Lines/whatCanIdo.png";
-import workSamplesLine from "../public/assets/Lines/work-samples.png";
-import workSamplesLine2 from "../public/assets/Lines/workSamplesLine2.png";
-import heroImage from "../public/assets/heroImage.png";
-import mobileDeveloper from "../public/assets/mobile-developer.png";
-import designer from "../public/assets/designer.png";
-import webDeveloper from "../public/assets/web-developer.png";
 import { TypeAnimation } from "react-type-animation";
-import Btn from "@/components/Btn";
-import ContinueLine from "@/components/ContinueLine";
-import WorkSampleCard from "@/components/WorkSampleCard";
-import OutlineBtn from "@/components/OutlineBtn";
+import {
+  Image,
+  navLine,
+  skillLine,
+  workSamplesLine,
+  workSamplesLine2,
+  heroImage,
+  mobileDeveloper,
+  designer,
+  webDeveloper,
+  Btn,
+  ContinueLine,
+  WorkSampleCard,
+  OutlineBtn,
+} from "./index";
 
 export default function Home() {
   return (
