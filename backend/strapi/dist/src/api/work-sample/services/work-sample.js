8f5e9cf9af227bfb48bd07e2ac54dc2134adbe14
Erfandev88 <erfangaaan@gmail.com>
2025-08-06 23:58:43 +0330

    remove strapi tmp and next build files

--- backend/strapi/dist/src/api/work-sample/services/work-sample.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * work-sample service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::work-sample.work-sample');
